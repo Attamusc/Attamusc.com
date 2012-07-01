30b569586c7311dcc40efa8fba6f17372eb24e32
Sean Dunn <sean@attamusc.com>
2012-07-01 15:50:18 -0400

    Switching from Ruby/Rails to Express

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AttaWebby::Application.config.secret_token = '57794fffdbbf840a4b0f6410c02a61a081b616b06b669c627961b8da561cd824fbafa320c10276b90d4338193be30e0f9bbabe22339f51871e6a0ed003e969d0'
